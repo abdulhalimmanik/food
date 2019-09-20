3504c09d00e74a11d3f05eee5c49d3d7047aa714
abdulhalimmanik <abdulhalimmanik@users.noreply.github.com>
2019-09-20 07:21:48 +0600

    Updates

---
+++ precache-manifest.19551485cacab9fed2ca8bef0457fdf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a7d88aba0a48fc949f467b90866490",
+    "url": "/food/index.html"
+  },
+  {
+    "revision": "24426073a96688561e17",
+    "url": "/food/static/css/main.a1a7495a.chunk.css"
+  },
+  {
+    "revision": "441f302d8b35af547e76",
+    "url": "/food/static/js/2.65a848fd.chunk.js"
+  },
+  {
+    "revision": "24426073a96688561e17",
+    "url": "/food/static/js/main.0ff184c0.chunk.js"
+  },
+  {
+    "revision": "83560b87f51014c484ca",
+    "url": "/food/static/js/runtime~main.db72a875.js"
+  }
+]);
